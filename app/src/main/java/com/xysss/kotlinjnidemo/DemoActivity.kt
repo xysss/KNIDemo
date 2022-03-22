ae1546f91ab4c130c2e36ea4f69d1465ed0a3ac9
xuyongsheng <1343025166@qq.com>
2022-03-22 16:27:04 +0800

    FCBR界面

---
+++ app/src/main/java/com/xysss/kotlinjnidemo/DemoActivity.kt
@@ -0,0 +1,11 @@
+package com.xysss.kotlinjnidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
